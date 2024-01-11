c4e679a71e121f6d67b5e6093ffba5aa6f0e82b3
RudraP272812 <rudrapatel201@gmail.com>
2024-01-11 15:44:33 -0500

    3d objects added

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <main className="bg-slate-300/20">
       <Router>
-        <Navbarar />
+        <Navbar />
         <Routes>
           <Route path="/" element ={<Home />}/>
           <Route path="/about" element ={<About />}/>
