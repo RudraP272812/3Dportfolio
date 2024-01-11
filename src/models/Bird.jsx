2a9eac6b4daf3cce11a920433be09e82287fb737
RudraP272812 <rudrapatel201@gmail.com>
2024-01-11 15:54:02 -0500

    bird and plane added with no animation

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import birdScene from '../assets/3d/bird.glb'
 import { useGLTF } from '@react-three/drei';
 const Bird = () => {
-    const bird = useGLTF(birdScene);
+    const {scene,animantion} = useGLTF(birdScene);
   return (
-    <mesh>
-      
+    <mesh position={[-5,2,1]} scale = {[0.003,0.003,0.003]}>
+      <primitive object={scene} />
     </mesh>
   )
 }
