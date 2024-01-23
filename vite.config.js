29f0372eaa0099300aaf5a29816eaea53778248c
RudraP272812 <rudrapatel201@gmail.com>
2024-01-23 11:48:08 -0500

    trying to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/RudraOfficialPortfolio/",
+  base:"/3Dportfolio/",
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
 })
